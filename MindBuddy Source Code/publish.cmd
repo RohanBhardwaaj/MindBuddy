383579f8bcf3df844613f0b4c534b6373a58db63
RohanBhardwaaj <69955535+RohanBhardwaaj@users.noreply.github.com>
2023-05-14 22:36:54 +0530

    Add files via upload

---
+++ MindBuddy Source Code/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotWithMSI.sln -p:DeployOnBuild=true -p:PublishProfile=.\MindBuddy-bot-ae8d-Web-Deploy.pubxml -p:Password=z8mSDLtPMhCNCwpQubjvGGRnqGvGq1BYcePjwbrrB8PRby70vvhqnK2zPJv9
+
